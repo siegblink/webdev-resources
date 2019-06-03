b7aea60f2c28b535e2c9aae382ca8485504b9bba
Siegfred Balona <siegblink@gmail.com>
2019-06-03 13:26:17 -0400

    Updates

---
+++ precache-manifest.889c33db4852ebd567b34be30479a230.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d4b9a85cb78be45c98d2f8ba0ca167f",
+    "url": "/webdev-resources/index.html"
+  },
+  {
+    "revision": "6305e866a2c869b3b8e2",
+    "url": "/webdev-resources/static/css/main.d9126894.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/webdev-resources/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "6305e866a2c869b3b8e2",
+    "url": "/webdev-resources/static/js/main.8c2b7b33.chunk.js"
+  },
+  {
+    "revision": "447b28ad72b176650cc5",
+    "url": "/webdev-resources/static/js/runtime~main.07213cb2.js"
+  }
+]);
